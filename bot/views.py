9d409a4f9cf10ec9c2d92ac01ae076f64d8ccfd9
himanish-tecblic <himanishvyas.tecblic@gmail.com>
2023-04-01 13:40:27 +0530

    automation api added

--- bot/views.py
+++ bot/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import pywhatkit
+import requests
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from bot.models import mssg
@@ -23,23 +24,42 @@ class home(APIView):
     #     a.save()
 
     def get(self,request):
-
+        lis = ["919724946042","917698544197"]
         
         # pywhatkit.sendwhatmsg("+919998699658", "test", "11", "16")
 
-        a = request.data["mobile"],
-        print("==========>>>", a)
-        b = request.data["msg"]
-        # sendwhatmsg_instantly
-        print("==============>>>>", b)
-        print(request.data["hour"])
-        print(request.data["min"])
+        # a = request.data["mobile"],
+        # print("==========>>>", a)
+        # b = request.data["msg"]
+        # # sendwhatmsg_instantly
+        # print("==============>>>>", b)
+        # print(request.data["hour"])
+        # print(request.data["min"])
 
-        print("----------------------------------------------------------a")
-        pywhatkit.sendwhatmsg(request.data["mobile"], request.data["msg"],
-                               request.data["hour"], request.data["min"])
-        print("----------------------------------------------------------b")
-        return Response("done ...")
+        # print("----------------------------------------------------------a")
+        # pywhatkit.sendwhatmsg(request.data["mobile"], request.data["msg"],
+        #                        request.data["hour"], request.data["min"])
+        # print("----------------------------------------------------------b")
+        for i in lis:
+                jsondata = {
+            "messaging_product": "whatsapp",
+            "to": i,
+            "type": "template",
+            "template": {
+                "name": "hi",
+                "language": {
+                    "code": "en_US"
+                }
+            }
+        }   
+                headers = {
+        
+                    "Authorization":"Bearer EAAQhSS35gHsBAHmPv5r3nOXVYyaOhagU6TEv4ZBms9iFegOkRNeefcjU7364AMiTghWZBGHffwJtfTiO9ZAlYtf5ZCnoSfZBfkZB6Ihgin18Qm7CI3r6mZCFVHksHIerGZBjkQfMdNoibUbZA0EpdHvM0hTgEMeEt2N179PjDt092j4qdeMaBRv0qGZAQfZAQKDS2U5UvQEiPWq6gZDZD"
+                    ,
+                    "Content-Type":"Application/json" 
+        }
+                
+                x = requests.post( "https://graph.facebook.com/v16.0/110807025306438/messages", json = jsondata,headers=headers)
+        return Response(x.json())
 
 
-# sdkhgfkshdfihkw
